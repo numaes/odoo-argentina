6b595c0a9b0a3ae2285a2d3051783f6bc761cddd
Daniel Blanco Martin <daniel@blancomartin.cl>
2014-12-30 16:27:06 -0300

    Modificaciones para resolver los siguientes temas, relativos a la compatiblidad chilena:
    - Incorporación del document type "RUT", como código 81 (no usado).
    - Colocación de este tipo de documento como documento por defecto.
    - Cuando la elección es RUT, transfiere el RUT al campo VAT
    - Contempla la existencia de la letra K para el RUT
    - Definir como inactivos varios tipos de  documentos de Argentina que no son de uso comun.
    - Mejorar el log del módulo l10n_ar_invoice durante el funcionamiento del onchange.

--- l10n_ar_invoice/partner.py
+++ l10n_ar_invoice/partner.py
@@ -7,9 +7,16 @@ class res_partner(models.Model):
     _inherit = 'res.partner'
 
     responsability_id = fields.Many2one(
-        'afip.responsability', 'Resposability')
+        'afip.responsability', 
+        'Resposability'
+    )
+    # para ejemplo
+    # default=lambda self: self._context.get('location_id', False)
+    # para ejemplo
     document_type_id = fields.Many2one(
-        'afip.document_type', 'Document type',)
+        'afip.document_type', 'Document type',
+        default=lambda self: self._context.get('document_type_id.name', 'RUT')
+        )
     document_number = fields.Char(
         'Document number', size=64,)
     iibb = fields.Char('Gross Income', size=64)
@@ -17,9 +24,22 @@ class res_partner(models.Model):
 
     @api.onchange('document_number', 'document_type_id')
     def onchange_document(self):
-        print '11111111'
+        print 'Entra al onchange del VAT'
         mod_obj = self.env['ir.model.data']
         if self.document_number and ('afip.document_type', self.document_type_id.id) == mod_obj.get_object_reference('l10n_ar_invoice', 'dt_CUIT'):
+            print 'entra por CUIT'
+            print 'self.document_type_id.id: ' + str(self.document_type_id.id)
             document_number = re.sub('[^1234567890]', '', str(self.document_number))
             self.vat = 'AR%s' % document_number
             self.document_number = document_number
+        ## agregado por Daniel Blanco
+        elif self.document_number and ('afip.document_type', self.document_type_id.id) == mod_obj.get_object_reference('l10n_ar_invoice', 'dt_RUT'):
+            print 'entra por RUT'
+            print 'self.document_type_id.id: ' + str(self.document_type_id.id)
+            print 'self.document_type_id.name: ' + str(self.document_type_id.name)
+            document_number = re.sub('[^1234567890K]', '', str(self.document_number))
+            self.vat = 'CL%s' % document_number
+            self.document_number = document_number
+        else:
+            print 'no entra por ninguno'
+            print 'self.document_type_id.id: ' + str(self.document_type_id.id)
